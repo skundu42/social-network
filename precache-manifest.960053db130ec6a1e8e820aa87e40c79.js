f2bd9bfd6b7715225b6c4c1b669c18578f1b47f9
skundu42 <skundu_be19@thapar.edu>
2021-04-16 22:42:22 +0530

    Updates

---
+++ precache-manifest.960053db130ec6a1e8e820aa87e40c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ba675245b0b96b5b24",
+    "url": "/social-network/static/css/main.71b53be5.chunk.css"
+  },
+  {
+    "revision": "b2ba675245b0b96b5b24",
+    "url": "/social-network/static/js/main.b2ba6752.chunk.js"
+  },
+  {
+    "revision": "dd86c68cddff415e17b7",
+    "url": "/social-network/static/css/1.fc88dfc0.chunk.css"
+  },
+  {
+    "revision": "dd86c68cddff415e17b7",
+    "url": "/social-network/static/js/1.dd86c68c.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "01fdcc2cb74e32edc3d89122fe835531",
+    "url": "/social-network/index.html"
+  }
+];
